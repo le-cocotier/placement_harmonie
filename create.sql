b8badfb4ae609d6c31edc0fdc8250f0b32a8790b
Julian63800 <pet.dog63800@gmail.com>
2024-11-25 22:20:17 +0100

    liste dependance

--- create.sql
+++ create.sql
@@ -26,6 +26,8 @@ insert into Partie(idPartie) values ('4');
 insert into Partie(idPartie) values ('barython');
 insert into Partie(idPartie) values ('tenor');
 insert into Partie(idPartie) values ('basse');
+insert into Partie(idPartie) values ('alto');
+
 
 INSERT INTO Personne(idInstrument, nomPersonne, prenomPersonne) values (1, 'BROCH', 'Sidonie');
 INSERT INTO Personne(idInstrument, nomPersonne, prenomPersonne) values (1, 'CLERC', 'Rémi');
@@ -78,7 +80,7 @@ INSERT INTO Personne(idInstrument, nomPersonne, prenomPersonne) values (4, 'ROUS
 INSERT INTO Personne(idInstrument, nomPersonne, prenomPersonne) values (6, 'BOZONNET', 'Cyril');
 INSERT INTO Personne(idInstrument, nomPersonne, prenomPersonne) values (6, 'LAVET', 'Pierre');
 INSERT INTO Personne(idInstrument, nomPersonne, prenomPersonne) values (6, 'CHADEYRON', 'Théo');
-INSERT INTO Personne(idInstrument, nomPersonne, prenomPersonne) values (6, 'VAYSSIERE', 'Solène');
+INSERT INTO Personne(idInstrument, nomPersonne, prenomPersonne) values (6, 'VAYSSIER', 'Solène');
 INSERT INTO Personne(idInstrument, nomPersonne, prenomPersonne) values (6, 'POTHERAT', 'Clément');
 INSERT INTO Personne(idInstrument, nomPersonne, prenomPersonne) values (6, 'PRESLE', 'Pauline');
 INSERT INTO Personne(idInstrument, nomPersonne, prenomPersonne) values (6, 'PUZIN', 'Tom');
@@ -100,3 +102,57 @@ INSERT INTO Personne(idInstrument, nomPersonne, prenomPersonne) values (11, 'GUI
 INSERT INTO Personne(idInstrument, nomPersonne, prenomPersonne) values (11, 'HAMONIC', 'Gaëlle');
 INSERT INTO Personne(idInstrument, nomPersonne, prenomPersonne) values (11, 'HÉLIAKIS', 'Thomas');
 INSERT INTO Personne(idInstrument, nomPersonne, prenomPersonne) values (11, 'RÉTY', 'Julian');
+
+/* Clarinette */
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (0, '1', 1, 1, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (0, '2', 1, 2, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (0, '3', 2, 3, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (0, 'alto', 1, 3, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (0, 'basse', 2, 3, 0);
+/* Saxophone */
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (3, '1', 1, 2, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (3, '2', 1, 2, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (3, 'tenor', 2, 3, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (3, 'barython', 2, 3, 0);
+/* Flute */
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (1, '1', 1, 2, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (1, '2', 1, 2, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (1, '3', 1, 2, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (1, '4', 1, 2, 0);
+/* Cor anglais / hautbois */
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (2, '1', 1, 1, 0);
+/* Basson */
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (5, '1', 2, 3, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (5, '2', 2, 3, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (5, '3', 2, 3, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (5, 'basse', 2, 3, 0);
+/* Cor */
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (4, '1', 2, 4, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (4, '2', 2, 4, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (4, '3', 2, 4, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (4, '4', 2, 4, 0);
+/* Cornet */
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (8, '1', 3, 4, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (8, '2', 3, 4, 0);
+/* Euphonium */
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (9, '1', 3, 4, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (9, '2', 3, 4, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (9, '3', 3, 4, 0);
+/* Tuba */
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (10, '1', 3, 5, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (10, '2', 3, 5, 0);
+/* Trompette */
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (6, '1', 5, 5, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (6, '2', 5, 5, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (6, '3', 5, 5, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (6, '4', 5, 5, 0);
+/* Trombone */
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (7, '1', 5, 5, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (7, '2', 5, 5, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (7, '3', 5, 5, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (7, '4', 5, 5, 0);
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (7, 'basse', 5, 5, 0);
+/* Contrebasse */
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (12, '1', 5, 6, 0);
+/* Percu */
+Insert into Se_decline(idInstrument, idPartie, rangMax, rangMin, leftPriority) values (11, '1', 6, 6, 0);
